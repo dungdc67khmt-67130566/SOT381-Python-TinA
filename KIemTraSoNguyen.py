ab47601ab583d2e94c3b8c63013d2fc1b506ca47
dungdc67khmt-67130566 <dung.dc.67khmt@ntu.edu.vn>
2025-12-07 21:39:47 +0700

    thêm bài kiểm tra số nguyên là số âm, số dương hay số 0

---
+++ KIemTraSoNguyen.py
@@ -0,0 +1,9 @@
+# Chuong trinh nhap 1 so nguyen tu ban phim
+# Kiem tra xem la so am,so duong hay so 0
+so = int(input("hay nhap mot so nguyen : "))
+if so>0 :
+    print("so duong")
+elif so<0 :
+    print("so am")
+else :
+    print("bang 0")
