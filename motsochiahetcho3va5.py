a302b5da93187dedfee2d045f0cab94b78054a4a
dungdc67khmt-67130566 <dung.dc.67khmt@ntu.edu.vn>
2025-12-08 14:02:59 +0700

    em đặt nhầm tên bài một số chia hết cho 3 và 5 thành bài trung bình cộng, em xin phép gửi lại

---
+++ motsochiahetcho3va5.py
@@ -0,0 +1,6 @@
+# nhap vao mot so nguyen kiem tra xem co chia het cho 3 va 5 hay khong
+n= int(input("nhap mot so : "))
+if n%3==0 and n%5==0 :
+    print("so nay chia het cho 3 va cho 5")
+else :
+    print("so nay khong chia het cho 3 va 5")
