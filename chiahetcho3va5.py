af1d18f2288b7288b8102b5b8f7734161553b9d9
dungdc67khmt-67130566 <dung.dc.67khmt@ntu.edu.vn>
2025-12-08 13:58:27 +0700

    kiểm tra thực hành

---
+++ chiahetcho3va5.py
@@ -0,0 +1,7 @@
+# nhập 3 so và tinh so trung binh cong
+a = int(input("nhap so a : "))
+b = int(input("nhap so b : "))
+c = int(input("nhap so c : "))
+tong_ba_so = a+b+c
+so_trung_binh_cong=tong_ba_so/3
+print(f"so trung binh cong la {so_trung_binh_cong}")
